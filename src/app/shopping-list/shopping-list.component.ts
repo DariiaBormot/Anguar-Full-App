4022416056b6f71c86e6d637da3b2b26cc20dd7e
dariia <dariiabormot@gmail.com>
2021-05-18 12:03:29 +0300

    Add recipe app components

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/models/ingredient.model'
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Flower', 50),
+    new Ingredient('Apple', 10)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
